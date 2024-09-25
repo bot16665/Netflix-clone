76a408a75cc4da74e5568f4fb8e267ac4b684974
faisal shaikh <azharshaikh40198@gmail.com>
2024-09-26 00:09:09 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,18 @@
+// replace below firebase config with your own
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCoHWWRpITCytoNzktr7b4vc8EnBn5-5M4",
+  authDomain: "netflix-clone-f676c.firebaseapp.com",
+  projectId: "netflix-clone-f676c",
+  storageBucket: "netflix-clone-f676c.appspot.com",
+  messagingSenderId: "109534649205",
+  appId: "1:109534649205:web:c9ef8a7da86e0e73fd913c"
+};
+
+
+// craete a/c on themoviedb.org and replace access token below
+
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ODAzNjEyZTc2OTlmNGM3ZGM4ZTc4YWI3YjJmODJlMCIsIm5iZiI6MTcyNjU2NTM2NS4yOTkwMjUsInN1YiI6IjY2ZTk0YTY0NWMwNTE5YTIzNGQyYzNkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.38z3wRrGrVuLPh1DaJI9IXo4Nhyj8ycb7xPhl9XYhGE";
+
+  export {firebaseConfig, TMDB_Access_Key};
